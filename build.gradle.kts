7cd62fcd370b0c6c79ab86dab4439a92d2ab05d2
ColdRain-Moro <rain@asgard.hk>
2022-03-03 23:53:08 +0800

    [feature] reconstruct PixivPics

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "kim.bifrost.rain.flandre"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -12,13 +12,13 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-    implementation("com.google.code.gson:gson:2.8.9")
-    api("net.mamoe:mirai-core:2.7.1")
-    api("net.mamoe:mirai-console:2.7.1")
+    implementation("com.google.code.gson:gson:2.9.0")
+    api("net.mamoe:mirai-core:2.9.2")
+    api("net.mamoe:mirai-console:2.9.2")
     implementation(kotlin("stdlib"))
-    testImplementation("net.mamoe:mirai-console-terminal:2.7.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("net.mamoe:mirai-console-terminal:2.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.getByName<Test>("test") {
--- src/main/kotlin/kim/bifrost/rain/flandre/PixivPics.kt
+++ src/main/kotlin/kim/bifrost/rain/flandre/PixivPics.kt
@@ -3,8 +3,8 @@ package kim.bifrost.rain.flandre
 import com.google.gson.Gson
 import kim.bifrost.rain.flandre.data.Illust
 import kim.bifrost.rain.flandre.data.SearchResult
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
@@ -12,14 +12,21 @@ import net.mamoe.mirai.console.data.value
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.Image
+import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
 import okhttp3.internal.closeQuietly
+import java.io.BufferedOutputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.net.URLEncoder
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 import kotlin.random.Random
 
 /**
@@ -31,24 +38,62 @@ import kotlin.random.Random
  **/
 object PixivPics : CoroutineScope by Flandre {
 
-    private val client = OkHttpClient()
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(20, TimeUnit.SECONDS)
+        .readTimeout(20, TimeUnit.SECONDS)
+        .build()
     private val gson = Gson()
-    private const val TAG = "フランドール･スカーレット"
-    private const val TAG_SUFFIX = "+1000users入り"
+
+    private val remapper = mutableMapOf(
+        "芙兰" to ("フランドール･スカーレット" to 1000),
+        "恋恋" to ("古明地こいし" to 1000),
+        "东方" to ("東方Project" to 5000),
+        "咲夜" to ("十六夜咲夜" to 1000),
+        "黑丝" to ("黒スト" to 10000),
+        "白丝" to ("白タイツ" to 10000),
+        "随便什么丝" to ("ストッキング" to 10000),
+        "涩图" to ("R-18" to 50000),
+        "风景" to ("風景" to 10000),
+        "女孩" to ("女の子" to 10000)
+    )
 
     fun init() {
         GlobalEventChannel.parentScope(this).subscribeAlways<GroupMessageEvent> {
-            if (message.content == "来张芙兰") {
+            val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
                 launch {
-                    if (Conf.apiKey.isEmpty()) {
-                        group.sendMessage("未配置apiKey.")
-                        return@launch
+                    group.sendMessage(message.quote().plus("发生错误: ${throwable.localizedMessage}"))
+                }
+            }
+            if (message.content.startsWith("来张")) {
+                val key = message.content
+                    .removePrefix("来张")
+                    .run {
+                        var str = this
+                        remapper.forEach { (k, v) ->
+                            str = str.replace(k, v.first)
+                        }
+                        str
                     }
-                    search().apply {
+                val limit = remapper[key]?.second ?: 1000
+                val query = key.run {
+                    var str = this
+                    remapper.forEach { (k, v) ->
+                        str = str.replace(k, v.first)
+                    }
+                    str
+                } + "+${limit}users入り"
+                launch(exceptionHandler) {
+                    search(query).apply {
+                        var tryCounts = 0
                         var item = get(Random.nextInt(size))
                         if (!Conf.allow_sex) {
                             while (item.sanity_level == 6) {
+                                if (tryCounts >= 3) {
+                                    group.sendMessage("不准涩涩！")
+                                    return@launch
+                                }
                                 item = get(Random.nextInt(size))
+                                tryCounts++
                             }
                         }
                         val url = if (item.page_count > 1) {
@@ -57,16 +102,18 @@ object PixivPics : CoroutineScope by Flandre {
                             item.meta_single_page.original_image_url
                         }.replace("\\/", "/")
                             .replace("i.pximg.net", "i.acgmx.com")
+                            .replace("_webp", "")
                         println(url)
-                        val img = getImage(url)?.toExternalResource() ?: return@launch let {
+                        val type = url.split(".").run { get(size - 1) }
+                        val img = getImage(url)?.toExternalResource(type) ?: return@launch let {
                             group.sendMessage("内部错误: 不存在的图片")
                         }
                         val imageId = img.uploadAsImage(group).imageId
                         img.closeQuietly()
                         val receipt = group.sendMessage(Image(imageId))
                         if (item.sanity_level == 6) {
-                            // 涩图将在30秒后撤回
-                            delay(TimeUnit.SECONDS.toMillis(30))
+                            // 涩图将在15秒后撤回
+                            delay(TimeUnit.SECONDS.toMillis(15))
                             receipt.recall()
                         }
                     }
@@ -75,25 +122,70 @@ object PixivPics : CoroutineScope by Flandre {
         }
     }
 
-    private suspend fun search(): List<Illust> = coroutineScope {
-        println()
+    private suspend fun search(key: String): List<Illust> = suspendCoroutine {
         val request = Request.Builder()
             .addHeader("token", Conf.apiKey)
-            .url("https://api.acgmx.com/public/search?q=${URLEncoder.encode(TAG + TAG_SUFFIX, "utf-8")}&offset=${Random.nextInt(100)}")
+            .url(
+                "https://api.acgmx.com/public/search?q=${
+                    URLEncoder.encode(
+                        key,
+                        "utf-8"
+                    )
+                }&offset=${Random.nextInt(100)}"
+            )
             .build()
-        val json = client.newCall(request)
-            .execute()
-            .body!!
-            .string()
-        val obj = gson.fromJson(json, SearchResult::class.java)
-        obj.illusts
+        client.newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    it.resumeWithException(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val obj = gson.fromJson(response.body!!.string(), SearchResult::class.java)
+                    it.resume(obj.illusts)
+                }
+
+            })
     }
 
-    private suspend fun getImage(url: String): ByteArray? = coroutineScope {
+    private suspend fun getImage(url: String): ByteArray? = suspendCoroutine {
         val request = Request.Builder()
             .url(url)
             .build()
-        client.newCall(request).execute().body?.bytes()
+        client.newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    it.resumeWithException(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    it.resume(if (!url.contains("_webp")) response.body?.bytes() else decodeWebP(response.body!!.bytes()))
+                }
+
+            })
+    }
+
+    private fun decodeWebP(encoded: ByteArray): ByteArray? {
+        var result:ByteArray? = null
+        val bitmapFactoryClazz = Class.forName("android.graphics.BitmapFactory")
+        val bitmapClazz = Class.forName("android.graphics.Bitmap")
+        val compressFormatClazz = Class.forName("android.graphics.Bitmap\$CompressFormat")
+        val decodeByteArrayMethod = bitmapFactoryClazz.getMethod("decodeByteArray", ByteArray::class.java, Int::class.java, Int::class.java) // public static Bitmap decodeByteArray(byte[] data, int offset, int length) {
+        val compressMethod = bitmapClazz.getMethod("compress", compressFormatClazz, Int::class.java, OutputStream::class.java)
+        val recycleMethod = bitmapClazz.getMethod("recycle")
+        val bitmap = decodeByteArrayMethod.invoke(null,encoded, 0, encoded.size)//BitmapFactory.decodeByteArray(encoded, 0, encoded.size)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        try {
+            val bos = BufferedOutputStream(byteArrayOutputStream)
+            compressMethod.invoke(bitmap, compressFormatClazz.getField("PNG").get(null), 100, bos)//bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos)
+            bos.flush()
+            result = byteArrayOutputStream.toByteArray()
+            bos.close()
+            recycleMethod.invoke(bitmap)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return result
     }
 
     object Conf : AutoSavePluginConfig("pixiv_pics") {
